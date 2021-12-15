a5032acfde657a116210dcf10d0170acff9380d1
Hira Akbar Ali <hiraremotescrach@gmail.com>
2021-12-15 09:54:54 +0500

    first commit

--- mylibrary/src/main/java/com/example/mylibrary/addition.java
+++ mylibrary/src/main/java/com/example/mylibrary/addition.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 public class addition extends AppCompatActivity {
 
     EditText ed1,ed2;
-    Button btn,btn2;
+    Button btn,btn2,btn_div,btn_sub,btn_mul;
     Double v1,v2,result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +26,9 @@ public class addition extends AppCompatActivity {
         ed2 = findViewById(R.id.ed2);
         btn = findViewById(R.id.btn_sum);
         btn2 = findViewById(R.id.btn_finish);
+        btn_div = findViewById(R.id.btn_div);
+        btn_mul = findViewById(R.id.btn_mul);
+        btn_sub = findViewById(R.id.btn_sub);
     }
 
     private void AllClickListener() {
@@ -39,7 +42,7 @@ public class addition extends AppCompatActivity {
                     v1 = Double.valueOf(ed1.getText().toString());
                     v2 = Double.valueOf(ed2.getText().toString());
                     result = v1 + v2;
-                    Toast.makeText(addition.this, "result" + result, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(addition.this, v1 + "+" +v2 + "=" + result, Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -51,11 +54,57 @@ public class addition extends AppCompatActivity {
                 Toast.makeText(addition.this, "There is nothing to show", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    Toast.makeText(addition.this, "result" + result, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(addition.this,"result"+result, Toast.LENGTH_SHORT).show();
 
                 }
                 finish();
             }
         });
+
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().isEmpty() || ed2.getText().toString().isEmpty()){
+                    Toast.makeText(addition.this, "Please input some value", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    v1 = Double.valueOf(ed1.getText().toString());
+                    v2 = Double.valueOf(ed2.getText().toString());
+                    result = v1 - v2;
+                    Toast.makeText(addition.this,  v1 + "-" +v2 + "=" + result + result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btn_mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().isEmpty() || ed2.getText().toString().isEmpty()){
+                    Toast.makeText(addition.this, "Please input some value", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    v1 = Double.valueOf(ed1.getText().toString());
+                    v2 = Double.valueOf(ed2.getText().toString());
+                    result = v1 * v2;
+                    Toast.makeText(addition.this,  v1 + "*" +v2 + "=" + result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btn_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ed1.getText().toString().isEmpty() || ed2.getText().toString().isEmpty()){
+                    Toast.makeText(addition.this, "Please input some value", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    v1 = Double.valueOf(ed1.getText().toString());
+                    v2 = Double.valueOf(ed2.getText().toString());
+                    result = v1 / v2;
+                    Toast.makeText(addition.this,  v1 + "/" +v2 + "=" + result, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
